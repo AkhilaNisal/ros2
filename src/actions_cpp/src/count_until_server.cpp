3692a626047d3b2ab1ccd6d1f427c6dee1d74b28
AkhilaNisal <wedamestrigean@gmail.com>
2026-02-08 03:50:43 +0530

    Update ros2_ws: Create new robot_arm, robot_arm_config_pkg, new_robot_arm_bringup for my custom robot arm.

--- src/actions_cpp/src/count_until_server.cpp
+++ src/actions_cpp/src/count_until_server.cpp
@@ -5,7 +5,7 @@
 using CountUntil = my_robot_interfaces::action::CountUntil;
 using CountUntilGoalHandle = rclcpp_action::ServerGoalHandle<CountUntil>;
 using namespace std::placeholders;
-
+ 
 class CountUntilServerNode : public rclcpp::Node
 {
     public:
@@ -33,36 +33,10 @@ class CountUntilServerNode : public rclcpp::Node
         {   
             (void)uuid;
             RCLCPP_INFO(this->get_logger(),"Recieved a goal"); 
-
-            //Policy: refuce new goal if one goal is being active
-            // {
-            //     std::lock_guard<std::mutex> lock(mutex_);
-            //     if (goal_handle_){
-            //         if (goal_handle_->is_active()){
-            //             RCLCPP_INFO(this->get_logger(),"A goal is still active, Rejecting the new goal"); 
-            //             return rclcpp_action::GoalResponse::REJECT;
-            //         }
-            //     }
-            // }
-
             if (goal->target_number <= 0){
                 RCLCPP_INFO(this->get_logger(),"Rejecting the goal"); 
                 return rclcpp_action::GoalResponse::REJECT;
             }
-
-            //Policy: preempt existing goal when receiving a new valid goal
-            {
-                std::lock_guard<std::mutex> lock(mutex_);
-                if (goal_handle_){
-                    if (goal_handle_->is_active()){
-                        RCLCPP_INFO(this->get_logger(),"Aborting current goal and accept new goal"); 
-                        preempted_goal_id_ = goal_handle_->get_goal_id();
-
-                    }
-                }
-            }
-            
-
             RCLCPP_INFO(this->get_logger(),"Accepting the goal"); 
             return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
         }
@@ -84,12 +58,7 @@ class CountUntilServerNode : public rclcpp::Node
 
         void execute_goal(
             const std::shared_ptr<CountUntilGoalHandle> goal_handle)
-        {   
-            {
-                std::lock_guard<std::mutex> lock(mutex_);
-                this->goal_handle_ = goal_handle;
-            }
-
+        {
             //get request from goal
             int target_number = goal_handle->get_goal()->target_number;
             double period = goal_handle->get_goal()->period;
@@ -101,14 +70,6 @@ class CountUntilServerNode : public rclcpp::Node
             rclcpp::Rate loop_rate(1.0/period);  // loops run in periodically
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
             for (int i = 0 ;i < target_number; i++){
-                {
-                    std::lock_guard<std::mutex> lock(mutex_);
-                    if (goal_handle->get_goal_id() == preempted_goal_id_){
-                        result->reached_number = counter;
-                        goal_handle->abort(result);
-                        return;
-                    }
-                }
                 if (goal_handle->is_canceling()){
                     result->reached_number = counter;
                     goal_handle->canceled(result);
@@ -132,9 +93,6 @@ class CountUntilServerNode : public rclcpp::Node
 
         rclcpp_action::Server<CountUntil>::SharedPtr count_until_server_;
         rclcpp::CallbackGroup::SharedPtr cb_group_;
-        std::mutex mutex_;
-        std::shared_ptr<CountUntilGoalHandle> goal_handle_;
-        rclcpp_action::GoalUUID preempted_goal_id_;
         
 };
 
